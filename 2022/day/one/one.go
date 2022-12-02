6a882ef584dbc3adffb6f97c26c309c99fd4f4f4
Curt Bushko <cbushko@gmail.com>
2022-12-02 00:38:30 -0500

    2022 day 1 complete

--- 2022/day/one/one.go
+++ 2022/day/one/one.go
@@ -1,1 +1,58 @@
 package one
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func MaxCalories(m map[int]string) (index, max int) {
+	var total int
+	var j int
+
+	for i, v := range m {
+		total = 0
+		fields := strings.Fields(v)
+		for _, element := range fields {
+			j, _ = strconv.Atoi(element)
+			total = total + j
+		}
+		if total > max {
+			max = total
+			index = i
+		}
+	}
+	return index, max
+}
+
+func TopThree(in string) int {
+	var index int
+	var max int
+	var total int
+
+	m := Splitter(in)
+
+	index, max = MaxCalories(m)
+	total = max
+	delete(m, index)
+
+	index, max = MaxCalories(m)
+	total = total + max
+	delete(m, index)
+
+	index, max = MaxCalories(m)
+	total = total + max
+	delete(m, index)
+
+	return total
+}
+
+func Splitter(str string) map[int]string {
+	m := make(map[int]string)
+	re := regexp.MustCompile("\n")
+	for i, line := range strings.Split(str, "\n\n") {
+		line = re.ReplaceAllString(line, " ")
+		m[i] = line
+	}
+	return m
+}
